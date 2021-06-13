be362959c76016db4c95422e69d15bfd56051a2f
AditiAgarwal <84237896+aditiagarwal30@users.noreply.github.com>
2021-06-13 16:05:43 +0530

    Create nCr.cpp

---
+++ Recursion/nCr.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int fact(int n)
+{
+ if(n==0)
+    return 1;
+ return fact(n-1)*n;
+}
+
+int nCr(int n,int r)  //by using fact function
+{
+ int num,den;
+ num=fact(n);
+ den=fact(r)*fact(n-r);
+ return num/den;
+}
+
+int NCR(int n,int r)  //by pascal triangle
+{
+ if(n==r || r==0)
+ return 1;
+ return NCR(n-1,r-1)+NCR(n-1,r);
+}
+
+int main()
+{
+ printf("%d \n",NCR(5,3));
+
+ printf("%d \n",nCr(5,3));
+ return 0;
+}
