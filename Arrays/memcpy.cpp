9906fd1f7158060b163206080241f016c79df318
AditiAgarwal <84237896+aditiagarwal30@users.noreply.github.com>
2021-06-14 21:16:08 +0530

    Create memcpy.cpp

---
+++ Arrays/memcpy.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+   const char src[50] = "http://www.tutorialspoint.com";
+   char dest[50];
+   strcpy(dest,"Heloooo!!");
+   printf("Before memcpy dest = %s\n", dest);
+   memcpy(dest, src, strlen(src)+1);
+   printf("After memcpy dest = %s\n", dest);
+   
+   return(0);
+}
