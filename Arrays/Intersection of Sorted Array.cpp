adb0b8bb5b0c02ebb428f526fe224e96ab389243
AditiAgarwal <84237896+aditiagarwal30@users.noreply.github.com>
2021-06-26 23:34:54 +0530

    Update Intersection of Sorted Array.cpp

--- Arrays/Intersection of Sorted Array.cpp
+++ Arrays/Intersection of Sorted Array.cpp
@@ -20,15 +20,19 @@ struct Array* Intersection(struct Array *arr1,struct Array *arr2)
  int i=0,j=0,k=0;
  struct Array *arr3=(struct Array *)malloc(sizeof(struct Array));
 
- for(i=0;j<arr1->length;i++)
- {  int m;
-    for(j=0;j<arr2->length;j++)
-        if(arr1->A[i]==arr2->A[j])
-        {
-            arr3->A[k++]=arr1->A[i];
-            break;
-        }
+ while(i<arr1->length && j<arr2->length)
+ {
+    if(arr1->A[i]<arr2->A[j])
+       i++;
+    else if(arr2->A[j]<arr1->A[i])
+       j++;
+    else if(arr1->A[i]==arr2->A[j])
+      {
+         arr3->A[k++]=arr1->A[i++];
+         j++;
+      }
  }
+
  arr3->length=k;
  arr3->size=10;
  return arr3;
