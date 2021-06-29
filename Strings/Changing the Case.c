0e1b762d3e8067bccdbada9669d305a454bd3608
AditiAgarwal <84237896+aditiagarwal30@users.noreply.github.com>
2021-06-29 17:33:01 +0530

    Update

--- Strings/Changing the Case.c
+++ Strings/Changing the Case.c
@@ -3,8 +3,8 @@
 int main()
 {   int i;
 	char s[]={'w','e','l','c','o','m','e','\0'};
-	char *p="ADITI AGARWAL";
-	char *m="Rishi Agarwal";
+	char p[]="ADITIAGARWAL";
+	char m[]="Rishi Agarwal";
 
 	for(i=0;s[i]!='\0';i++)
 	{
