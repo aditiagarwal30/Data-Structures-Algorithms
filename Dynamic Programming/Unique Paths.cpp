899b88b586a9e7abc6023344e25d0e035c63f8c7
Aditi Agarwal <aditi2k1@gmail.com>
2024-05-12 17:57:31 +0530

    Create Unique Paths.cpp

---
+++ Dynamic Programming/Unique Paths.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid)
+    {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (obstacleGrid[i][j] == 1)
+                    dp[i][j] = 0;
+                else if (i == 0 and j == 0)
+                    dp[i][j] = 1;
+                else
+                {
+                    int up = 0, left = 0;
+                    if (i > 0)
+                        up = dp[i - 1][j];
+                    if (j > 0)
+                        left = dp[i][j - 1];
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
